e49ddc0e6261392909151a7fc0883576dc80ab1c
backtend <backtend@gmail.com>
2022-04-09 22:15:15 +0800

    优化探针缓存时间为1秒钟

--- app/index/controller/IndexController.php
+++ app/index/controller/IndexController.php
@@ -10,13 +10,11 @@ class IndexController extends BaseController
     public function index()
     {
         try {
-            $probing = \think\facade\Cache::store('file')->get('probing');
+            if ($probing = \think\facade\Cache::store('file')->get(__METHOD__)) {
+                return $this->restful(200, $probing, 'Cache Info');
+            }
         } catch (\Throwable $e) {
             return $this->restful(502, 'Cache Failed');
-        }
-
-        if ($probing) {
-            return $this->restful(200, $probing, 'Cache Info');
         }
 
         //phpinfo();die;
@@ -83,11 +81,9 @@ class IndexController extends BaseController
         $info['MYSQL_TIMEZONE'] = implode('/', array_column(\think\facade\Db::query('show variables LIKE "%time_zone%";'), 'Value'));
 
         //记入缓存
-        \think\facade\Cache::store('file')->set('probing', $info, 3);
+        \think\facade\Cache::store('file')->set(__METHOD__, $info, 1);
 
         return $this->restful(200, $info, sprintf('hey.%s', time()));
-        //LAST.captcha-20210321
-
-        //return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V' . \think\facade\App::version() . '<br/><span style="font-size:30px;">14载初心不改 - 你值得信赖的PHP框架</span></p><span style="font-size:25px;">[ V6.0 版本由 <a href="https://www.yisu.com/" target="yisu">亿速云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ee9b1aa918103c4fc"></think>';
     }
+    //LAST.probing-20220409
 }
